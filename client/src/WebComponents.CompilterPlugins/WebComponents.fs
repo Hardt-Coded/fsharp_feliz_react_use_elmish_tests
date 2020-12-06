22d953125216b778aaca454c459605c55ecf2f9d
Daniel Hardt <info@hardt-solutions.de>
2020-12-06 10:23:51 +0100

    shadow dom option added

--- client/src/WebComponents.CompilterPlugins/WebComponents.fs
+++ client/src/WebComponents.CompilterPlugins/WebComponents.fs
@@ -28,18 +28,9 @@ type ReactWebComponentAttribute(exportDefault: bool) =
             // F# Component()
             // JSX <Component />
             // JS createElement(Component, null)
-
-
-
-            //Fable.Sequential [
-            //    Fable.Let (AstUtils.makeIdent "elem", (AstUtils.makeCall (AstUtils.makeImport "createElement" "react") [callee; info.Args.[0] ]), Fable.Expr.Sequential [])
-            //]
-
             (AstUtils.makeCall (AstUtils.makeImport "createElement" "react") [callee; info.Args.[0] ])
-            
         | _ ->
             // return expression as is when it is not a call expression
-            compiler.LogWarning("end up in | _ -> ")
             expr
     
     override this.Transform(compiler, file, decl) =
@@ -110,9 +101,11 @@ type ReactWebComponentAttribute(exportDefault: bool) =
 
 
 
-type CreateReactWebComponentAttribute(customElementName:string) =
+type CreateReactWebComponentAttribute(customElementName:string, userShadowDom:bool) =
     inherit MemberDeclarationPluginAttribute()
     override _.FableMinimumVersion = "3.0"
+
+    new(customElementName:string) = CreateReactWebComponentAttribute(customElementName, true)
 
     override _.TransformCall(compiler, memb, expr) =
         expr
@@ -162,7 +155,7 @@ type CreateReactWebComponentAttribute(customElementName:string) =
                                         reactFunctionWithPropsBody; 
                                         AstUtils.makeImport "default" "react"
                                         AstUtils.makeImport "default" "react-dom"
-                                        AstUtils.emitJs "{ shadow: true }" []
+                                        AstUtils.emitJs (sprintf "{ shadow: %s }" (if isShadowDom then "true" else "false") []
                                     ]
                 
                 
@@ -181,68 +174,4 @@ type CreateReactWebComponentAttribute(customElementName:string) =
         | _ ->
             compiler.LogError "The imput for the web component must be a react element function generated from [<ReactWebComponents>]!"
             decl
-
-
-
-            //if (AstUtils.isCamelCase decl.Name) then
-            //    compiler.LogWarning(sprintf "React function component '%s' is written in camelCase format. Please consider declaring it in PascalCase (i.e. '%s') to follow conventions of React applications and allow tools such as react-refresh to pick it up." decl.Name (AstUtils.capitalize decl.Name))
-    
-            //// do not rewrite components accepting records as input
-            //if decl.Args.Length = 1 && AstUtils.isRecord compiler decl.Args.[0].Type then
-            //    // check whether the record type is defined in this file
-            //    // trigger warning if that is case
-            //    let definedInThisFile =
-            //        file.Declarations
-            //        |> List.tryPick (fun declaration ->
-            //            match declaration with
-            //            | Declaration.ClassDeclaration classDecl ->
-            //                let classEntity = compiler.GetEntity(classDecl.Entity)
-            //                match decl.Args.[0].Type with
-            //                | Fable.Type.DeclaredType (entity, genericArgs) ->
-            //                    let declaredEntity = compiler.GetEntity(entity)
-            //                    if classEntity.IsFSharpRecord && declaredEntity.FullName = classEntity.FullName
-            //                    then Some declaredEntity.FullName
-            //                    else None
-    
-            //                | _ -> None
-    
-            //            | Declaration.ActionDeclaration action ->
-            //                None
-            //            | _ ->
-            //                None
-            //        )
-    
-            //    match definedInThisFile with
-            //    | Some recordTypeName ->
-            //        let errorMsg = String.concat "" [
-            //            sprintf "Function component '%s' is using a record type '%s' as an input parameter. " decl.Name recordTypeName
-            //            "This happens to break React tooling like react-refresh and hot module reloading. "
-            //            "To fix this issue, consider using use an anonymous record instead or use multiple simpler values as input parameters"
-            //            "Future versions of [<ReactComponent>] might not emit this warning anymore, in which case you can assume that the issue if fixed. "
-            //            "To learn more about the issue, see https://github.com/pmmmwh/react-refresh-webpack-plugin/issues/258"
-            //        ]
-    
-            //        compiler.LogWarning(errorMsg, ?range=decl.Body.Range)
-    
-            //    | None ->
-            //        // nothing to report
-            //        ignore()
-    
-            //    { decl with ExportDefault = exportDefault }
-            //else if decl.Args.Length = 1 && decl.Args.[0].Type = Fable.Type.Unit then
-            //    // remove arguments from functions requiring unit as input
-            //    { decl with Args = [ ]; ExportDefault = exportDefault }
-            //else
-            //    compiler.LogError "ReactWebComponents only accept one anonymous record or unit as parameter."
-            //    decl
-            
-
-        //if decl.Info.IsValue || decl.Info.IsGetter || decl.Info.IsSetter then
-            
-        //else if not (AstUtils.isReactElement decl.Body.Type) then
-            
-        //else
-            
-
-
    
